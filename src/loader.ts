17c1691024d44b89f5e76eea348df60a3d4b6379
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2023-07-27 22:58:41 -0300

    Starting project but my gpt key account ended kkkkk

---
+++ src/loader.ts
@@ -0,0 +1,43 @@
+import path from 'node:path';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { JSONLoader } from 'langchain/document_loaders/fs/json';
+import { TokenTextSplitter } from 'langchain/text_splitter';
+import { RedisVectorStore } from 'langchain/vectorstores/redis';
+import { createClient } from 'redis';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+
+const loader = new DirectoryLoader(
+    path.resolve(__dirname, '../tmp'),
+    {
+        '.json': patch => new JSONLoader(patch, '/Story')
+    }
+)
+
+async function load() {
+    const docs = await loader.load();
+    const splitter = new TokenTextSplitter({
+        encodingName: 'cl100k_base',
+        chunkSize: 600,
+        chunkOverlap: 0 // Não quero que o conteudo de um chunk apareça no  outro
+    })
+
+    const splitterDocuments = await splitter.splitDocuments(docs);
+    const redis = createClient({
+        url: 'redis://127.0.0.1:6379'
+    });
+
+    const redisconn = await redis.connect();
+
+    console.log(redisconn)
+
+    await RedisVectorStore.fromDocuments(splitterDocuments,
+        new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
+        {   indexName: 'story-darksouls',
+            redisClient: redis,
+            keyPrefix: 'story:'
+        });
+
+    await redis.disconnect();
+}
+
+load();
